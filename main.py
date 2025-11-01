a3b98b03791ed0e99328ec55268fa9f1c26e4319
MathieuLaronce <laroncemathieu@gmail.com>
2025-11-01 21:37:36 +0100

    ajout creation de la bdd_sqlite

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from etl import run_etl
+import subprocess
+import sys
 
 def main():
     print("Lancement du pipeline VeriPix")
-    run_etl()
-    print("ETL terminé avec succès !")
+
+    # Création / reset de la base
+    print("Création de la base")
+    subprocess.run([sys.executable, "creation_sqlite.py"])
+
+    # ELT complet (extract → load → transform)
+    print("Lancement de l'ETL")
+    subprocess.run([sys.executable, "etl.py"])
+
+    print("Fin")
 
 if __name__ == "__main__":
     main()
