fa592fca0d5581ff75d7d2d491bca2e8a6337886
MathieuLaronce <laroncemathieu@gmail.com>
2025-11-05 16:11:41 +0100

    main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import sys
 def main():
     print("Lancement du pipeline VeriPix")
 
-    # Création / reset de la base
+    # Création 
     print("Création de la base")
     subprocess.run([sys.executable, "creation_sqlite.py"])
 
-    # ELT complet (extract → load → transform)
+    # ELT complet (extract load transform)
     print("Lancement de l'ETL")
     subprocess.run([sys.executable, "etl.py"])
 
